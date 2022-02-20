bcafff77ea68dad9235a22a9adc11274ee374d01
AymericPost <aymeric.post@gmail.com>
2022-02-20 14:04:21 +0100

    Security checks on remote script

--- ibmbackup.py
+++ ibmbackup.py
@@ -1,7 +1,14 @@
-from clients.ssh import get_ssh_client
-from config.argsparser import namespace
+from time import sleep
 
-if __name__ == "__main__":    
+from config.argsparser import namespace
+from clients.ssh import get_ssh_client
+from utils.sumcheck import checksum
+
+if __name__ == "__main__":
     ssh = get_ssh_client(namespace.host, namespace.user, namespace.private_key)
 
+    # Checks if remote script exists and is the same as local script
+    checksum(ssh, namespace.exec_path)
+
     ssh.close()
+    del ssh
